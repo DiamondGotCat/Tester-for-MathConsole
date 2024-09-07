388da77a1958b5603c3d5e5507d5a330063d58fa
DiamondGotCat <124330624+DiamondGotCat@users.noreply.github.com>
2024-09-07 18:47:26 +0900

    Update install.py

--- install.py
+++ install.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 # 取得したいURL
 url = "https://diamondgotcat.github.io/Tester-for-MathConsole/main.py"
@@ -9,7 +10,8 @@ response = requests.get(url)
 # レスポンスのステータスコードを確認
 if response.status_code == 200:
     # レスポンスのテキストデータをファイルに書き込む
-    with open("output.txt", "w", encoding="utf-8") as file:
+    os.mkdir("/usr/local/share/mathconsole/plugins/tester/main.py")
+    with open("/usr/local/share/mathconsole/plugins/", "w", encoding="utf-8") as file:
         file.write(response.text)
     print("Installed")
 else:
